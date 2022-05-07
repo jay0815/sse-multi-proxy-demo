9b8b41020961996521ecf0281d51301244cbdf5c
jack <543612779@qq.com>
2022-05-07 11:31:31 +0800

    update: sse server response data in random

--- backend/src/router/stream.ts
+++ backend/src/router/stream.ts
@@ -4,20 +4,23 @@ import SSE from '../core/sse';
 const router = Router();
 
 router.get<'/:id', { id: string }>('/:id', (request, response) => {
-    const timer = request.params.id;
-    const localSSE = new SSE([`${timer} hello!`])
+    const ts = request.params.id;
+    const localSSE = new SSE([`${ts} hello!`])
     localSSE.init(request, response);
     let count = 0;
     let tm = setInterval(() => {
+        const random = Math.floor(Math.random() * 10);
         localSSE.send(JSON.stringify({
-            data: [1,2,3,4,5,6,7],
-            timer,
+            data: new Array(random).fill(0).map((_, i) => i),
+            ts,
+            timer: new Date().valueOf()
         }), 'data');
         count++;
         if (count === 2) {
             localSSE.send(JSON.stringify({
                 isEnd: true,
-                timer
+                ts,
+                timer: new Date().valueOf()
             }), 'data');
             clearInterval(tm);
         }
