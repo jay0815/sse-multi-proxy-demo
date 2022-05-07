c3a04f3269080fd6f60c5f987c3eb14ea6eb57ad
jack <543612779@qq.com>
2022-05-07 11:43:53 +0800

    update: add sleep before send close flag

--- backend/src/router/stream.ts
+++ backend/src/router/stream.ts
@@ -3,12 +3,15 @@ import SSE from '../core/sse';
 
 const router = Router();
 
+
+const sleep = (t: number) => new Promise((resolve) => setTimeout(() => resolve(void 0), t));
+
 router.get<'/:id', { id: string }>('/:id', (request, response) => {
     const ts = request.params.id;
     const localSSE = new SSE([`${ts} hello!`])
     localSSE.init(request, response);
     let count = 0;
-    let tm = setInterval(() => {
+    let tm = setInterval(async () => {
         const random = Math.floor(Math.random() * 10);
         localSSE.send(JSON.stringify({
             data: new Array(random).fill(0).map((_, i) => i),
@@ -16,7 +19,8 @@ router.get<'/:id', { id: string }>('/:id', (request, response) => {
             timer: new Date().valueOf()
         }), 'data');
         count++;
-        if (count === 2) {
+        if (count === 5) {
+            await sleep(3000);
             localSSE.send(JSON.stringify({
                 isEnd: true,
                 ts,
